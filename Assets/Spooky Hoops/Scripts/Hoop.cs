70df0da897ee25df70fe93635642aa2cb4027442
ElDuderini <Justin_Peters@live.com>
2020-10-09 21:35:53 -0600

    implementeted various changest to make a new agent

---
+++ Assets/Spooky Hoops/Scripts/Hoop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoop : MonoBehaviour
+{
+
+    private Collider innerCollider;
+    private Collider ringCollider;
+
+    //private HoopSpawner spawner;
+
+
+    private void Awake()
+    {
+        innerCollider = GetComponent<BoxCollider>();
+        ringCollider = GetComponent<MeshCollider>();
+    }
+
+    public Vector3 HoopUpVector
+    {
+        get
+        {
+            return innerCollider.transform.up;
+        }
+    }
+
+    public Vector3 RingCenterPosition
+    {
+        get
+        {
+            return innerCollider.transform.position;
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "Agent")
+    //    {
+    //        Destroy(this);
+    //    }
+    //}
+
+
+    //private void OnDestroy()
+    //{
+    //    spawner.SpawnHoop();
+    //    spawner.hoops.RemoveAll(item => item == null);
+    //}
+}
